ce6bf6c60226f5a408302253580882e9e773a3be
hsh <hsh200315@naver.com>
2024-02-16 10:55:02 +0900

    style: 스위치 디자인 및 상황 글씨크기 수정

--- frontend/spark_page/src/components/Switch.jsx
+++ frontend/spark_page/src/components/Switch.jsx
@@ -11,7 +11,10 @@ export default function SwitchComponent({isOn, setisOn}){
     <>
       <div className='ToggleContainer' onClick={toggleHandler}>
 				<div className='ToggleBox'>
-					<div className={`toggle-container ${isOn ? "toggle--checked" : null}`}/>
+					<div className={`toggle-container ${isOn ? "toggle--checked" : null}`}>
+						<div className='smallBox'>원작</div>
+						<div className='smallBox'>상황</div>
+					</div>
 					{isOn === false ? <div className={`toggle-circle ${isOn ? "toggle--checked" : null}`}>원작</div> : <div className={`toggle-circle ${isOn ? "toggle--checked" : null}`}>상황</div>}
 				</div>
       </div>
