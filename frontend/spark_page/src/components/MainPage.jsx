407c4c7e7fd905b046c6c6cda042a693ad673a11
hsh <hsh200315@naver.com>
2024-02-20 23:11:29 +0900

    style: 문구 수정 및 글씨체 수정

--- frontend/spark_page/src/components/MainPage.jsx
+++ frontend/spark_page/src/components/MainPage.jsx
@@ -15,15 +15,14 @@ export default function MainPage(){
 	return(
 		<div className='MainPageBackGround'>
 			<div className='MainPageLargeTextSet'>
-				<h1 className='MainPageLargeText'>Fiction</h1>
+				<h1 className='MainPageLargeText'>차차</h1>
 				<div className='TextBox'>
-					<img src={Pencil} width={38} height={38} style={{marginTop:'2%'}}></img>
-					<h1 className='MainPageLargeText'>Comes True!</h1>
+					{/* <img src={Pencil} width={38} height={38} style={{marginTop:'2%'}}></img> */}
+					<h1 className='MainPageLargeText2'>Chat Character</h1>
 				</div>
-				
 			</div>
 			<div className='MainPageSmallTextSet'>
-				<h2 className='MainPageSmallText'>좋아하는 웹툰 등장 인물과</h2>
+				<h2 className='MainPageSmallText'>좋아하는 캐릭터와</h2>
 				<h2 className='MainPageSmallText'>다양한 상황에서 대화해보세요!</h2>
 			</div>
 			<Button variant='primary' className='StartButton' onClick={handleClick}>등장 인물 찾아보기</Button>
