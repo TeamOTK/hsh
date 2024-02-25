577b1125ea6920c9822f6f3bd5bd629f797e1a04
hsh <hsh200315@naver.com>
2024-02-26 01:14:32 +0900

    feat: 캐릭터 공유 백 연결 및 검색 기능 구현

--- frontend/spark_page/src/components/CharacterCommunity.jsx
+++ frontend/spark_page/src/components/CharacterCommunity.jsx
@@ -1,5 +1,6 @@
-import { useNavigate } from 'react-router-dom';
-import { useState  } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useEffect, useState  } from 'react';
+import axios from 'axios';
 
 import Button from 'react-bootstrap/esm/Button';
 import './CharacterCommunity.css'
@@ -10,18 +11,42 @@ import Bam from '../images/Bam.png'
 
 export default function CharacterCommunity(){
 	const navigate = useNavigate();
+	const location = useLocation();
 
-	const onClickButton = () => {
-		navigate('/setting/character');
+	const [characterList, setCharacterList] = useState('');
+	const [searchInput, setSearchInput] = useState('');
+	const [filteredData,setFilteredData] = useState()
+
+	const userId = location.state.userId;
+	const getCharacters = async () => {
+		const res = await axios.get("http://13.209.167.220/characters/list");
+		// 답장
+		setCharacterList(res.data.character);
+		setFilteredData(res.data.character)
 	}
 
-	const onClickItem = () => {
-		navigate('/setting/character')
+	const searchItems = (searchValue) => {
+		setSearchInput(searchValue)
+		setFilteredData(characterList.filter((item) => {
+			return Object.values(item).join('').toLowerCase().includes(searchValue.toLowerCase())
+		}))
+		console.log(filteredData)
+	}
+	useEffect(() =>{
+		getCharacters();
+	},[])
+
+	const onClickButton = () => {
+		navigate('/setting/character', {state: {userId: userId}});
+	}
+
+	const onClickItem = (character) => {
+		navigate('/setting/character', {state: {userId: userId, isCommunity:true, name:character.name, setting: character.setting, img:character.img, personality: character.personality, accent: character.accent, characterId: character.character_id}})
 	}
 
 	return(
 		<>
-		<div className='WarningHeader'>
+			<div className='WarningHeader'>
 				<BsChevronLeft size={25} onClick={onClickButton}/>
 				<h2 className="text"></h2>
 				{/* <BsSearch size={30} style={{marginRight:'3%',fontWeight:'bold'}} onClick={handleClickSearch}/> */}
@@ -34,43 +59,24 @@ export default function CharacterCommunity(){
 			<div className='SubWarning'>캐릭터는 사용자의 창작물입니다.</div>
 			<div className='SearchContainer'>
 				<BsSearch size={24}></BsSearch> 
-				<input className='CommunitySearch'></input>
+				<input className='CommunitySearch' onChange={(e) => searchItems(e.target.value)}></input>
 			</div>
 			<div className='CharacterSetList'>
-				<div className='CharacterSetItem'>
-					<img src={Bam} className='CommunityItemImg'/>
-					<div className='CommunityItemInfo'>
-						<div className='CommunityItemTopBox'>
-							<div style={{fontWeight:"bold", fontSize:"140%", marginRight:'-24%'}}>청명</div>
-							<div style={{fontSize:'85%'}}>누적 사용자 30</div>
-							<div className='CommunityItemBtn' onClick={onClickItem}>사용하기</div>
+				{filteredData && filteredData.map((character) => {
+					return(
+						<div className='CharacterSetItem' key={character.character_id}>
+						<img src={`https://chacha-spark.s3.ap-northeast-2.amazonaws.com/character/${character.img}`} className='CommunityItemImg'/>
+						<div className='CommunityItemInfo'>
+							<div className='CommunityItemTopBox'>
+								<div style={{fontWeight:"bold", fontSize:"140%", marginRight:'-24%'}}>{character.name}</div>
+								<div style={{fontSize:'85%'}}>누적 사용자 {character.user_cnt}</div>
+								<div className='CommunityItemBtn' onClick={() => onClickItem(character)}>사용하기</div>
+							</div>
+							<div className='ItemDesc'>{character.setting}</div>
 						</div>
-						<div className='ItemDesc'>여기에 설명이 들어감여기에 설명이 들어감여기에 설명이 들어감</div>
 					</div>
-				</div>
-				<div className='CharacterSetItem'>
-					<img src={Bam} className='CommunityItemImg'/>
-					<div className='CommunityItemInfo'>
-						<div className='CommunityItemTopBox'>
-							<div style={{fontWeight:"bold", fontSize:"140%", marginRight:'-24%'}}>청명</div>
-							<div style={{fontSize:'85%'}}>누적 사용자 30</div>
-							<div className='CommunityItemBtn' onClick={onClickItem}>사용하기</div>
-						</div>
-						<div className='ItemDesc'>여기에 설명이 들어감여기에 설명이 들어감여기에 설명이 들어감</div>
-					</div>
-				</div>
-				<div className='CharacterSetItem'>
-					<img src={Bam} className='CommunityItemImg'/>
-					<div className='CommunityItemInfo'>
-						<div className='CommunityItemTopBox'>
-							<div style={{fontWeight:"bold", fontSize:"140%", marginRight:'-24%'}}>청명</div>
-							<div style={{fontSize:'85%'}}>누적 사용자 30</div>
-							<div className='CommunityItemBtn' onClick={onClickItem}>사용하기</div>
-						</div>
-						<div className='ItemDesc'>여기에 설명이 들어감여기에 설명이 들어감여기에 설명이 들어감</div>
-					</div>
-				</div>
-				
+					)
+				})}
 			</div>
 		</div>
 		</>
